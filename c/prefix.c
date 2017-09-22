441305d78e5b74835f342f6500b0130879f9d11d
Sven Wick <swick@CNU4109DNK.abm-local.de>
2017-09-22 19:28:35 +0200

    prefix

---
+++ c/prefix.c
@@ -0,0 +1,21 @@
+#include <glib.h>
+
+int main(void) {
+
+   if ( g_str_has_prefix("foo", "f") ) {
+
+       g_print("\n\tfoo starts with a f");
+
+   }
+
+   if ( g_str_has_prefix("bar", "f") ) {
+
+       g_print("\n\tbar starts with a f");
+
+   } else {
+
+       g_print("\n\tbar starts NOT with a f\n\n");
+
+    }
+
+}
