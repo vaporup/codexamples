91195bc0a4cac1007278b228d4d01a44caf108c4
Sven Wick <swick@thinkcentre.gnu.local>
2017-09-22 04:11:44 +0200

    environment variables

--- c/env.c
+++ c/env.c
@@ -4,14 +4,27 @@
 
 int main() {
 
+  g_print("\n");
+
   gchar **all_env;
   all_env = g_get_environ();
-  g_print("2nd environment var: ");
+  g_print("%-20s\t: ", "2nd environment var");
   g_print("%s\n", all_env[1]);
 
   const gchar *env;
   env = g_environ_getenv(all_env, "LANG");
-  g_print("LANG var: ");
+  g_print("%-20s\t: ", "LANG");
   g_print("%s\n", env);
 
+  const gchar *another_env;
+  another_env = g_getenv("LANG");
+  g_print("%-20s\t: ", "LANG");
+  g_print("%s\n", another_env);
+
+  const gchar *user_config_dir;
+  user_config_dir = g_get_user_config_dir();
+  g_print("%-20s\t: ", "USER__CONFIG_DIR");
+  g_print("%s\n", user_config_dir);
+
+  g_print("\n");
 }
