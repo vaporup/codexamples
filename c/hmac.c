f9ca26ef202566660b8c0d027cba6ca8c576da05
Sven Wick <swick@thinkcentre.gnu.local>
2017-09-23 23:48:10 +0200

    more hmac examples

--- c/hmac.c
+++ c/hmac.c
@@ -1,25 +1,35 @@
 #include <glib.h>
+#include <string.h>
 
 gchar *my_hmac;
 
+const guchar *key = "foo";
+const gchar  *data = "bar";
+
+guint key_length = 0;
+guint data_length = 0;
+
 int main (void) {
+
+  key_length = strlen(key);
+  data_length = strlen(data);
 
   g_print("\n");
 
   // MD5
-  my_hmac = g_compute_hmac_for_string(G_CHECKSUM_MD5, (guchar*)"foo", 3, "bar", -1);
+  my_hmac = g_compute_hmac_for_string(G_CHECKSUM_MD5, key, key_length, data, data_length);
   g_print("%10s: %s\n", "MD5", my_hmac);
 
   // SHA1
-  my_hmac = g_compute_hmac_for_string(G_CHECKSUM_SHA1, (guchar*)"foo", 3, "bar", -1);
+  my_hmac = g_compute_hmac_for_string(G_CHECKSUM_SHA1, key, key_length, data, data_length);
   g_print("%10s: %s\n", "SHA1", my_hmac);
 
   // SHA256
-  my_hmac = g_compute_hmac_for_string(G_CHECKSUM_SHA256, (guchar*)"foo", 3, "bar", -1);
+  my_hmac = g_compute_hmac_for_string(G_CHECKSUM_SHA256, key, key_length, data, data_length);
   g_print("%10s: %s\n", "SHA256", my_hmac);
 
   // SHA512
-  my_hmac = g_compute_hmac_for_string(G_CHECKSUM_SHA512, (guchar*)"foo", 3, "bar", -1);
+  my_hmac = g_compute_hmac_for_string(G_CHECKSUM_SHA512, key, key_length, data, data_length);
   g_print("%10s: %s\n", "SHA512", my_hmac);
 
   g_print("\n");
