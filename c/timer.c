c8d08b0cfb93befd137db372d54d82fa4fb5c289
Sven Wick <swick@thinkcentre.gnu.local>
2017-09-23 14:28:06 +0200

    just use the print function from glib. no need for <stdio.h>

--- c/timer.c
+++ c/timer.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <glib.h>
  
@@ -10,10 +9,10 @@ int main(int argc, char* argv[])
 
     timer = g_timer_new();
 
-    printf("Sleeping for 5 seconds...\n");
+    g_print("Sleeping for 5 seconds...\n");
     sleep(5);
     elapsed = g_timer_elapsed(timer, NULL);
-    printf("I'm awake!!! I slept for %f s\n", elapsed);
+    g_print("I'm awake!!! I slept for %f s\n", elapsed);
  
     return 0;
 }
