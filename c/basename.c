0e2823466a6f50ded9fee3646c0416d31ec23312
Sven Wick <swick@thinkcentre.gnu.local>
2017-09-23 14:56:04 +0200

    formatting

--- c/basename.c
+++ c/basename.c
@@ -3,9 +3,7 @@
 int main(void) {
 
     gchar *basename;
-
     basename = g_path_get_basename("/tmp/foo/bar.txt");
-
-   g_print("\n\t /tmp/foo/bar.txt's basename is %s\n\n", basename);
+    g_print("\n\t /tmp/foo/bar.txt's basename is %s\n\n", basename);
 
 }
